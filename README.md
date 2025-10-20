db354b869982ce73b710328b2b463e974da23151
randomizedcoder dave.seddon.ca@gmail.com <dave.seddon.ca@gmail.com>
2025-10-20 10:48:58 -0700

    writeShellApplication

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Use Nixidy and ArgoCD to manage kubernetes cluster
 
 ![](assets/nixidy-ci-cd.png)
+
+Blog using this repo
+https://tech.aufomm.com/gitops-for-kubernetes-with-nixidy-and-argocd/
+
+Video about the blog
+https://youtu.be/3XtcSXbLh-c?si=Yh4EDY6AfaEmBbOq
+
+See also
+https://github.com/arnarg/nixidy
--- flake.nix
+++ flake.nix
@@ -80,11 +80,14 @@
       apps = forEachSystem (
         system: pkgs: {
           build = {
-            type = "app";
-            program = toString (
-              pkgs.writeScript "build-yamls" # bash
+            type = "app"; # bash
+            program = toString pkgs.writeShellApplication {
+              name = "build-yamls";
+              runtimeInputs = with pkgs; [
+                self.packages.${system}.nixidy
+              ];
+              text =
                 ''
-                  #!${pkgs.bash}/bin/bash
                   set -e
 
                   # Default to "all" if no argument provided
@@ -97,9 +100,9 @@
                     # Build all environments
                     for env in $available_envs; do
                       echo "Generating App manifests for $env..."
-                      ${self.packages.${system}.nixidy}/bin/nixidy switch .#$env
+                      nixidy switch .#$env
                       echo "Generating App of Apps for $env..."
-                      ${self.packages.${system}.nixidy}/bin/nixidy bootstrap .#$env > manifests/$env/bootstrap.yaml
+                      nixidy bootstrap .#$env > manifests/$env/bootstrap.yaml
                     done
                   else
                     # Check if the specified environment exists
@@ -110,33 +113,39 @@
                         break
                       fi
                     done
-                    
+
                     if [ "$env_found" = "true" ]; then
                       echo "Generating App manifests for $target_env..."
-                      ${self.packages.${system}.nixidy}/bin/nixidy switch .#$target_env
+                      nixidy switch .#$target_env
                       echo "Generating App of Apps for $target_env..."
-                      ${self.packages.${system}.nixidy}/bin/nixidy bootstrap .#$target_env > manifests/$target_env/bootstrap.yaml
+                      nixidy bootstrap .#$target_env > manifests/$target_env/bootstrap.yaml
                     else
                       echo "Error: Environment '$target_env' not found."
                       echo "Available environments: $available_envs"
                       exit 1
                     fi
                   fi
-                ''
-            );
+                '';
+            };
           };
 
           init = {
-            type = "app";
-            program = toString (
-              pkgs.writeScript "init-cluster" # bash
+            type = "app"; # bash
+            program = toString pkgs.writeShellApplication {
+              name = "init-cluster";
+              runtimeInputs = with pkgs; [
+                kubectl
+                gnused
+                gnugrep
+                coreutils # for base64
+              ];
+              text =
                 ''
-                  #!${pkgs.bash}/bin/bash
                   set -e
                   echo "Installing sops-secrets-operator on cluster"
 
                   # Create namespace and secret
-                  ${pkgs.kubectl}/bin/kubectl apply -f - <<EOF
+                  kubectl apply -f - <<EOF
                   apiVersion: v1
                   kind: Namespace
                   metadata:
@@ -157,11 +166,11 @@
                     key.txt: $(base64 -w 0 < key.txt)
                   EOF
 
-                  ${pkgs.kubectl}/bin/kubectl apply -f manifests/infra/sops-secrets-operator
-                  ${pkgs.kubectl}/bin/kubectl rollout status -n sops-operator deployment sops-sops-secrets-operator
+                  kubectl apply -f manifests/infra/sops-secrets-operator
+                  kubectl rollout status -n sops-operator deployment sops-sops-secrets-operator
 
                   echo "Installing Cert Manager"
-                  ${pkgs.kubectl}/bin/kubectl apply -f manifests/infra/k8s-gw-api-crds
+                  kubectl apply -f manifests/infra/k8s-gw-api-crds
 
                   cm_max_retries=5
                   cm_retry_delay=10
@@ -170,12 +179,12 @@
 
                   for ((i=1; i<=cm_max_retries; i++)); do
                       echo "Attempt $i: Applying cert-manager manifests..."
-                      if ${pkgs.kubectl}/bin/kubectl apply -f manifests/infra/cert-manager; then
+                      if kubectl apply -f manifests/infra/cert-manager; then
                           echo "Cert-manager applied successfully. Checking ClusterIssuer 'lab-k8s-ca-issuer' readiness..."
 
                           for ((j=1; j<=issuer_max_retries; j++)); do
                               echo "Attempt $j: Checking if ClusterIssuer 'lab-k8s-ca-issuer' is ready..."
-                              if ${pkgs.kubectl}/bin/kubectl get clusterissuer lab-k8s-ca-issuer -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}' 2>/dev/null | grep -q "True"; then
+                              if kubectl get clusterissuer lab-k8s-ca-issuer -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}' 2>/dev/null | grep -q "True"; then
                                   echo "ClusterIssuer 'lab-k8s-ca-issuer' is ready!"
                                   break 2 # Break both loops
                               else
@@ -198,9 +207,9 @@
                   fi
 
                   echo "Installing ArgoCD on cluster"
-                  ${pkgs.kubectl}/bin/kubectl apply -f manifests/infra/argocd
-                  ${pkgs.kubectl}/bin/kubectl rollout status -n argocd deployment argocd-server
-                  ${pkgs.kubectl}/bin/kubectl apply -f manifests/infra/bootstrap.yaml
+                  kubectl apply -f manifests/infra/argocd
+                  kubectl rollout status -n argocd deployment argocd-server
+                  kubectl apply -f manifests/infra/bootstrap.yaml
 
                   # Wait for ArgoCD to sync and the deployment to be ready
                   namespace="kuma-system"
@@ -213,10 +222,10 @@
                   # Retry loop
                   for ((i=1; i<=max_retries; i++)); do
                       echo "Attempt $i: Checking if deployment $deployment exists in namespace $namespace..."
-                      if ${pkgs.kubectl}/bin/kubectl get deployment -n "$namespace" "$deployment" &>/dev/null; then
+                      if kubectl get deployment -n "$namespace" "$deployment" &>/dev/null; then
                           # Deployment exists, now check rollout status
                           echo "Deployment found. Checking rollout status..."
-                          if ${pkgs.kubectl}/bin/kubectl rollout status -n "$namespace" deployment "$deployment"; then
+                          if kubectl rollout status -n "$namespace" deployment "$deployment"; then
                               echo "Deployment is ready!"
                               break
                           fi
@@ -231,9 +240,9 @@
                       echo "Deployment $deployment in namespace $namespace was not ready after $((max_retries * retry_delay)) seconds."
                       exit 1
                   fi
-                  ${pkgs.kubectl}/bin/kubectl apply -f manifests/dev/bootstrap.yaml
-                ''
-            );
+                  kubectl apply -f manifests/dev/bootstrap.yaml
+                '';
+            };
           };
         }
       );
